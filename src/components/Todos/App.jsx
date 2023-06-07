1e2e9d2b51c750f10c0e8940c610e581cf8c3f9c
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-07 17:44:07 +0400

    adde editable function in form

--- src/components/Todos/App.jsx
+++ src/components/Todos/App.jsx
@@ -1,52 +1,111 @@
-import React, { useState, useEffect } from 'react'
-import Form from './Form'
-import Students from './Students'
-import moment from 'moment'
-
+import React, { useState } from 'react'
+import data from './data.json'
+import MyForm from './MyForm'
+import Users from './Users'
 import './App.scss'
 
 export default function App() {
-	const [students, setStudents] = useState(JSON.parse(localStorage.getItem('students') || '[]'))
+  const [users, setUsers] = useState(data)
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const {
-			firstname,
-			lastname,
-			email,
-			languages } = e.target;
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		const student = {
-			id: Date.now(),
-			firstname: firstname.value,
-			lastname: lastname.value,
-			email: email.value,
-			languages: languages.value,
-			registeredDate: moment()
-		}
-		setStudents(prevStudents => {
-			return [...prevStudents, student]
-		})
-		
-		
-		e.target.reset()
-	}
-	useEffect(() => {
-		localStorage.setItem('students', JSON.stringify(students))
-	}, [students])
-	
+    const {
+      username,
+      lastname,
+      email,
+      cities,
+      female,
+      male,
+    } = e.target;
 
-	return (
-		<div className='container'>
-			<div className="container-info">
+    const newUser = {
+      id: Date.now(),
+      first_name: username.value,
+      last_name: lastname.value,
+      email: email.value,
+      gender: (female.checked && female.value)
+        || (male.checked && male.value),
+      ip_address: cities.value,
+    }
+    setUsers([...users, newUser]);
+    e.target.reset()
+  }
 
-			</div>
-			<div className='container-form'>
-				<Form onSubmit={handleSubmit} />
-			</div>
-			<div className='container-students'>
-				<Students students={students} />
-			</div>
-		</div>
-	)
+  const handleDelete = (id) => {
+    const filtered = users.filter(elem => elem.id !== id);
+    setUsers(filtered)
+  }
+
+  const handleClear = () => {
+    setUsers([])
+  }
+
+  const handleSorted = (state, value) => {
+    const sorted = [...users]
+    if (state) {
+      sorted.sort((a, b) => a[value].localeCompare(b[value]));
+    } else {
+      sorted.sort((a, b) => b[value].localeCompare(a[value]));
+    }
+    setUsers(sorted)
+
+  }
+
+  const filteredGender = (gender) => {
+    const fill = data.filter(user => user.gender === gender);
+    setUsers(fill)
+  }
+
+  const [editableID, setEditableID] = useState(null)
+
+  const seteditableIdAction = (userId) => {
+    setEditableID(userId)
+  }
+
+  const EditableHandleSubmit = (e, id) => {
+    seteditableIdAction(null)
+    e.preventDefault();
+    console.log(id);
+    const { first_name, last_name, email, gender, ip_address } = e.target;
+    const user = {
+      id,
+      first_name: first_name.value || '',
+      last_name: last_name.value || '',
+      email: email.value || '',
+      gender: gender.value || '',
+      ip_address: ip_address.value || '',
+    }
+    console.log(user);
+    const newUsers = users.map(elem => {
+      if (elem.id === id) {
+        return { ...elem, user };
+      }
+      return elem;
+    })
+    setUsers(newUsers)
+  }
+
+  return (
+    <div className='wrapper'>
+      <header>
+        <button className='delButton' onClick={handleClear}>Clear All</button>
+        <p>Users <span>{users.length}</span></p>
+      </header>
+      <div className="wrapper-form">
+        <MyForm handleSubmit={handleSubmit} />
+      </div>
+      <div className="wrapper-table">
+        <Users
+          users={users}
+          handleDelete={handleDelete}
+          handleSorted={handleSorted}
+          filteredGender={filteredGender}
+          handleSubmit={EditableHandleSubmit}
+          editableID={editableID}
+          seteditableIdAction={seteditableIdAction}
+        />
+      </div>
+    </div>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/example-10(axios-albums)/App'
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './components/Todos/App'
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-reportWebVitals();
