e38cc8d8f50b134ceace9143dd23e3acc6271409
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-16 00:10:16 +0400

    add pagination example with package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/example-12(add-to-card)/App'
+import App from './components/example-6(paginantion)/withPackage/App'
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
